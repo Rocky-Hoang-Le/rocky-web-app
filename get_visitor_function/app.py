fec4d66c3f965ad3a47cc7f081ceeb305269e54d
Rocky Le <rockyhoangle@gmail.com>
2021-06-13 09:34:07 -0400

    unzip visitor function

---
+++ get_visitor_function/app.py
@@ -0,0 +1,50 @@
+import simplejson as json
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table_name = 'CountVisitors'
+table = dynamodb.Table(table_name)
+ID = '0'
+
+
+# import requests
+# testing github actions deploy
+
+def lambda_handler(event, context):
+    response = table.get_item(
+        Key={
+            'ID': ID
+        }
+    )
+
+    if 'Item' in response:
+        response = table.update_item(
+            Key={
+                'ID': ID
+            },
+            UpdateExpression="set visitorcount = visitorcount + :N",
+            ExpressionAttributeValues={
+                ':N': 1
+            }
+        )
+        response = table.get_item(
+            Key={
+                'ID': ID
+            }
+        )
+        visitsresponse = {
+            "statusCode": 200,
+            'headers': {
+                        'Access-Control-Allow-Headers': '*',
+                        'Access-Control-Allow-Origin': '*',
+                        'Access-Control-Allow-Methods': 'GET'
+            },
+            "body": json.dumps({
+                        "counter": response['Item'].get('visitorcount'),
+            }),
+        }
+        visits = json.loads(visitsresponse["body"])
+        return visits["counter"]
+    else:
+        print("There was an error")
+
